60c40657851cf57f9694cf0df32ae4d8a2714899
AK-PHOENIX <tigeraman55555@gmail.com>
2025-06-10 11:32:56 +0530

    fisrt

---
+++ js/script.js
@@ -0,0 +1,291 @@
+jQuery("#testi").owlCarousel({
+  autoplay: true,
+  rewind: false, /* use rewind if you don't want loop */
+  margin: 20,
+  loop: true,
+   /*
+  animateOut: 'fadeOut',
+  animateIn: 'fadeIn',
+  */
+  responsiveClass: true,
+  autoHeight: true,
+  autoplayTimeout: 3000,
+  smartSpeed: 800,
+  nav: false,
+  navText: [
+    '<svg width="50" height="50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>',
+    '<svg width="50" height="50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg>'],
+  responsive: {
+    0: {
+      items: 1
+    },
+
+    600: {
+      items: 1
+    },
+
+    1024: {
+      items: 1
+    },
+
+    1366: {
+      items: 1
+    }
+  }
+});
+jQuery("#blog").owlCarousel({
+  autoplay: true,
+  rewind: false, /* use rewind if you don't want loop */
+  margin: 20,
+  loop: true,
+   /*
+  animateOut: 'fadeOut',
+  animateIn: 'fadeIn',
+  */
+  responsiveClass: true,
+  autoHeight: true,
+  autoplayTimeout: 3000,
+  smartSpeed: 800,
+  dots: true,
+  nav: false,
+  navText: [
+    '<svg width="50" height="50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>',
+    '<svg width="50" height="50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg>'],
+  responsive: {
+    0 :{
+      items: 1
+    },
+    480: {
+      items: 1
+    },
+    767:{
+      items: 1
+    },
+
+    991: {
+      items: 3
+    },
+
+    1199: {
+      items: 3
+    },
+
+    1366: {
+      items: 3
+    }
+  }
+});
+
+jQuery("#ser").owlCarousel({
+  autoplay: true,
+  rewind: false, /* use rewind if you don't want loop */
+  margin: 20,
+  loop: true,
+   /*
+  animateOut: 'fadeOut',
+  animateIn: 'fadeIn',
+  */
+  responsiveClass: true,
+  autoHeight: true,
+  autoplayTimeout: 3000,
+  smartSpeed: 800,
+  nav: false,
+  dots:false,
+  navText: [
+    '<svg width="50" height="50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>',
+    '<svg width="50" height="50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg>'],
+  responsive: {
+    0: {
+      items: 1
+    },
+
+    767: {
+      items: 2
+    },
+    992: {
+      items: 3
+    },
+
+    1024: {
+      items: 5
+    },
+
+    1366: {
+      items: 5
+    }
+  }
+});
+jQuery(".brands-ser").owlCarousel({
+  autoplay: true,
+  rewind: false, /* use rewind if you don't want loop */
+  margin: 20,
+  loop: true,
+   /*
+  animateOut: 'fadeOut',
+  animateIn: 'fadeIn',
+  */
+  responsiveClass: true,
+  autoHeight: true,
+  autoplayTimeout: 3000,
+  smartSpeed: 800,
+  nav: false,
+  dots:false,
+  navText: [
+    '<svg width="50" height="50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>',
+    '<svg width="50" height="50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg>'],
+  responsive: {
+    0: {
+      items: 2
+    },
+
+    767: {
+      items: 3
+    },
+
+    1024: {
+      items: 5
+    },
+
+    1366: {
+      items: 5
+    }
+  }
+});
+jQuery("#partner").owlCarousel({
+  autoplay: true,
+  rewind: false, /* use rewind if you don't want loop */
+  margin: 20,
+  loop: true,
+   /*
+  animateOut: 'fadeOut',
+  animateIn: 'fadeIn',
+  */
+  responsiveClass: true,
+  autoHeight: true,
+  autoplayTimeout: 3000,
+  smartSpeed: 800,
+  nav: false,
+  dots:false,
+  navText: [
+    '<svg width="50" height="50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>',
+    '<svg width="50" height="50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg>'],
+  responsive: {
+    0: {
+      items: 2
+    },
+
+    767: {
+      items: 3
+    },
+
+    1024: {
+      items: 5
+    },
+
+    1366: {
+      items: 5
+    }
+  }
+});
+jQuery("#testis").owlCarousel({
+    autoplay: true,
+    rewind: false, /* use rewind if you don't want loop */
+    margin: 20,
+    loop: true,
+     /*
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    */
+    responsiveClass: true,
+    autoHeight: true,
+    autoplayTimeout: 3000,
+    smartSpeed: 800,
+    nav: false,
+    dots:true,
+    navText: [
+      '<svg width="50" height="50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>',
+      '<svg width="50" height="50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg>'],
+    responsive: {
+      0: {
+        items: 1
+      },
+  
+      767: {
+        items: 1
+      },
+  
+      1024: {
+        items: 1
+      },
+  
+      1366: {
+        items: 1
+      }
+    }
+  });
+jQuery(".service").owlCarousel({
+    autoplay: true,
+    rewind: false, /* use rewind if you don't want loop */
+    margin: 20,
+    loop: true,
+     /*
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    */
+    responsiveClass: true,
+    autoHeight: true,
+    autoplayTimeout: 3000,
+    smartSpeed: 800,
+    nav: true,
+    dots:false,
+    navText: [
+      '<svg width="50" height="50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>',
+      '<svg width="50" height="50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg>'],
+    responsive: {
+      0: {
+        items: 1
+      },
+  
+      767: {
+        items: 1
+      },
+  
+      1024: {
+        items: 1
+      },
+  
+      1366: {
+        items: 1
+      }
+    }
+  });
+
+  const circularProgress = document.querySelectorAll(".circular-progress");
+
+  Array.from(circularProgress).forEach((progressBar) => {
+    const progressValue = progressBar.querySelector(".percentage");
+    const innerCircle = progressBar.querySelector(".inner-circle");
+    let startValue = 0,
+      endValue = Number(progressBar.getAttribute("data-percentage")),
+      speed = 50,
+      progressColor = progressBar.getAttribute("data-progress-color");
+  
+    const progress = setInterval(() => {
+      startValue++;
+      progressValue.textContent = `${startValue}%`;
+      progressValue.style.color = `${progressColor}`;
+  
+      innerCircle.style.backgroundColor = `${progressBar.getAttribute(
+        "data-inner-circle-color"
+      )}`;
+  
+      progressBar.style.background = `conic-gradient(${progressColor} ${
+        startValue * 3.6
+      }deg,${progressBar.getAttribute("data-bg-color")} 0deg)`;
+      if (startValue === endValue) {
+        clearInterval(progress);
+      }
+    }, speed);
+  });
+  
+  
